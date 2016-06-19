40980b93a73d000038a09a9ca260561097b95384
Bernard Bolter <BeMajor@Bernards-MacBook-Pro.local>
2016-06-19 15:07:31 -0700

    version 1 for the live dev server

--- footer.php
+++ footer.php
@@ -1,6 +1,40 @@
     <footer>
-      <h2 class="footer_title">Salon le Rouge</h2>
-      <h3 class="footer_address">1375 Burlingame Ave., Ste 205, Burlingame, CA 94010</h2>
+      <div class="footer_text">
+        <h2 class="footer_title">Salon le Rouge</h2>
+        <h3 class="footer_address">1375 Burlingame Ave., Ste 205, Burlingame, CA 94010</h2>
+      </div>
+
+
+      <div class="social_links">
+        <a href="" alt="instagram" class="instagram">
+          <div class="svg-container svg-container-instagram">
+            <svg class="svg" viewBox="0 0 30 30" preserveAspectRatio="xMinYMin meet">
+              <use xlink:href="#svg-instagram"></use>
+            </svg>
+          </div>
+        </a>
+        <a href="" alt="facebook" class="facebook">
+          <div class="svg-container svg-container-facebook">
+            <svg class="svg" viewBox="0 0 30 30" preserveAspectRatio="xMinYMin meet">
+              <use xlink:href="#svg-facebook"></use>
+            </svg>
+          </div>
+        </a>
+        <a href="" alt="twitter" class="twitter">
+          <div class="svg-container svg-container-twitter">
+            <svg class="svg" viewBox="0 0 30 30" preserveAspectRatio="xMinYMin meet">
+              <use xlink:href="#svg-twitter"></use>
+            </svg>
+          </div>
+        </a>
+        <a href="" alt="yelp" class="yelp">
+          <div class="svg-container svg-container-yelp">
+            <svg class="svg" viewBox="0 0 64 30" preserveAspectRatio="xMinYMin meet">
+              <use xlink:href="#svg-yelp"></use>
+            </svg>
+          </div>
+        </a>
+      </div>
     </footer>
     <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/mashup.js"></script>
   </body>
--- video.php
+++ video.php
@@ -1,5 +1,9 @@
-<video autoplay  poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/polina.jpg" id="bgvid" loop>
+<video autoplay poster="<?php bloginfo('template_directory'); ?>/videos/rouge_test.png" id="bgvid" loop>
   <!-- WCAG general accessibility recommendation is that media such as background video play through only once. Loop turned on for the purposes of illustration; if removed, the end of the video will fade in the same way created by pressing the "Pause" button  -->
-<source src="//demosthenes.info/assets/videos/polina.webm" type="video/webm">
-<source src="//demosthenes.info/assets/videos/polina.mp4" type="video/mp4">
+  <source src="<?php bloginfo('template_directory'); ?>/videos/rouge_test.webm" type="video/webm">
+  <source src="<?php bloginfo('template_directory'); ?>/videos/rouge_test.ogv" type="video/ogg">
+  <source src="<?php bloginfo('template_directory'); ?>/videos/rouge_test.mp4" type="video/mp4">
 </video>
+<div class="video_overlay">
+  <h1>A touch of European Luxury in the Heart of Burlingame</h1>
+</div>
